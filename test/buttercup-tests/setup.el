6824d2691c172b76def92a8bb8293d2b98304a3a
ianbattersby <ian.battersby@gmail.com>
2015-07-29 22:52:41 +0100

    Local changes.

--- test/buttercup-tests/setup.el
+++ test/buttercup-tests/setup.el
@@ -212,8 +212,7 @@ with one."
 
 ;;; Test suite setup. Start a test server process that can be used by
 ;;; all tests
-(let ((omnisharp-server-executable-path (concat omnisharp-emacs-root-path
-                                                "/omnisharp-roslyn/omnisharp")))
+(let ((omnisharp-server-executable-path "/home/ian/code/omnisharp-roslyn/scripts/Omnisharp"))
   (omnisharp--create-ecukes-test-server omnisharp-emacs-root-path))
 ;; wait that the server is alive and ready before starting the test run
 (with-timeout (2 ; seconds
